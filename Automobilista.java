9690cfeed27add3589fa86985860e26c4b20a319
fabioromano1 <51378941+fabioromano1@users.noreply.github.com>
2019-06-05 15:56:16 +0200

    Add files via upload

---
+++ Automobilista.java
@@ -0,0 +1,31 @@
+
+public class Automobilista {
+
+	private Automobile automobile;
+	private Ticket ticket;
+	
+	
+	public Automobilista(Automobile automobile) {
+		this.automobile = automobile;
+		ticket = null;
+	}
+
+	public void ritiraTicket(Ticket ticket) {
+		this.ticket = ticket;
+	}
+	
+	public Ticket getTicket() {
+		return ticket;
+	}
+	
+	public Automobile lasciaAutomobile() {
+		Automobile auto = automobile;
+		automobile=null;
+		return auto;
+	}
+	
+	public void ritira (Automobile auto) {
+		automobile = auto;
+	}
+	
+}
